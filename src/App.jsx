3dace22ba15a7b567c1ef5d11da3b5a2d856e4d3
Vishrut <165580939+Vishrut7080@users.noreply.github.com>
2025-04-18 23:28:09 +0530

    Create a reference to the image tag.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
+// all of them are hooks
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
 
   const [img, setImg] = useState()
 
+  const imgRef = useRef()
+
   function handleChange(e) {
     const file = e.target.files[0]
+    // to access url of the uploaded image that is stored in browser
     const imgURL = URL.createObjectURL(file)
     console.log(imgURL)
     setImg(imgURL)
   }
+
+  // hook in react
+  useEffect(() => {
+    console.log(imgRef)
+  }, [img])
 
   return (
     <div>
@@ -19,8 +28,8 @@ function App() {
         <label htmlFor="image">Image: </label>
         <input id="image" type="file" onChange={handleChange} />
       </form>
-
-      <img src={img} alt="user selected image" />
+      {/* reference(acts similar to id in html) in react */}
+      <img ref={imgRef} src={img} alt="user selected image" />
 
       <div>
         <p>Result: </p>
